64004a2f5803dc3e51cd0e052c2cfbbdb60dbc4f
Yuriy Maslov <yuriy.maslof@gmail.com>
2017-01-30 13:52:09 +0300

    Curl fixed

--- helpers/Services.php
+++ helpers/Services.php
@@ -1,34 +1,41 @@
 <?php
 function sendTelegram( $email = null, $message = null )
 {
-    $response = \Curl::to(env('SERVICES_HOST') . '/api/telegram')
-        ->withData([
+    $client = new GuzzleHttp\Client;
+
+    $accessToken = env('SERVICES_TOKEN');
+
+    $response = $client->request('POST', env('SERVICES_HOST') . '/api/telegram', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ],
+        'form_params' => [
             'email'   => $email,
             'message' => $message,
-        ])
-        ->withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer ' . env('SERVICES_TOKEN'),
-        ])
-        ->post();
+        ],
+    ]);
+
     return $response;
 }
 
-function getAuthToken($grant_type = 'client_credentials', $client_id = null, $client_secret = null, $scope = null)
+function getServicesToken($grant_type = 'client_credentials', $client_id = null, $client_secret = null, $scope = null)
 {
     if( empty($client_id) || empty($client_secret) ) {
         $client_id = env('SERVICES_CLIENT_ID');
         $client_secret = env('SERVICES_CLIENT_SECRET');
     }
-    
-    $response = \Curl::to(env('SERVICES_HOST') . '/oauth/token')
-        ->withData([
+
+    $guzzle = new GuzzleHttp\Client;
+
+    $response = $guzzle->post(env('SERVICES_HOST') . '/oauth/token', [
+        'form_params' => [
             'grant_type' => $grant_type,
             'client_id' => $client_id,
             'client_secret' => $client_secret,
             'scope' => $scope,
-        ])
-        ->post();
+        ],
+    ]);
 
     return $response;
 }
