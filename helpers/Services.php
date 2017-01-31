5aac14313d479079509aa21f20f28745587c6190
Yuriy Maslov <yuriy.maslof@gmail.com>
2017-01-31 14:53:15 +0300

    New methods

--- helpers/Services.php
+++ helpers/Services.php
@@ -1,4 +1,25 @@
 <?php
+function getServicesToken($grant_type = 'client_credentials', $client_id = null, $client_secret = null, $scope = null)
+{
+    if( empty($client_id) || empty($client_secret) ) {
+        $client_id = env('SERVICES_CLIENT_ID');
+        $client_secret = env('SERVICES_CLIENT_SECRET');
+    }
+
+    $guzzle = new GuzzleHttp\Client;
+
+    $response = $guzzle->post(env('SERVICES_HOST') . '/oauth/token', [
+        'form_params' => [
+            'grant_type' => $grant_type,
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
+            'scope' => $scope,
+        ],
+    ]);
+
+    return $response;
+}
+
 function sendTelegram( $email = null, $message = null )
 {
     $client = new GuzzleHttp\Client;
@@ -19,21 +40,44 @@ function sendTelegram( $email = null, $message = null )
     return $response;
 }
 
-function getServicesToken($grant_type = 'client_credentials', $client_id = null, $client_secret = null, $scope = null)
+function sendSms( $phone = null, $message = null )
 {
-    if( empty($client_id) || empty($client_secret) ) {
-        $client_id = env('SERVICES_CLIENT_ID');
-        $client_secret = env('SERVICES_CLIENT_SECRET');
-    }
+    $client = new GuzzleHttp\Client;
 
-    $guzzle = new GuzzleHttp\Client;
+    $accessToken = env('SERVICES_TOKEN');
 
-    $response = $guzzle->post(env('SERVICES_HOST') . '/oauth/token', [
+    $response = $client->request('POST', env('SERVICES_HOST') . '/api/telegram', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ],
         'form_params' => [
-            'grant_type' => $grant_type,
-            'client_id' => $client_id,
-            'client_secret' => $client_secret,
-            'scope' => $scope,
+            'phone'   => $phone,
+            'message' => $message,
+            'template_id' => 'test',
+            'json'    => 'test'
+        ],
+    ]);
+
+    return $response;
+}
+
+function sendEmail( $email = null, $message = null )
+{
+    $client = new GuzzleHttp\Client;
+
+    $accessToken = env('SERVICES_TOKEN');
+
+    $response = $client->request('POST', env('SERVICES_HOST') . '/api/telegram', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ],
+        'form_params' => [
+            'email'   => $email,
+            'message' => $message,
+            'template_id' => 'test',
+            'json'    => 'test'
         ],
     ]);
 
